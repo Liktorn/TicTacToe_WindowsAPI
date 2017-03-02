1dc4968c85a16840a9ea95e6b08bff689cb1f461
Björn Winnerstam <brevbomb@hotmail.com>
2017-03-02 15:48:21 +0100

    first

--- TicTacToe_WindowsAPI/TicTacToe_WindowsAPI.cpp
+++ TicTacToe_WindowsAPI/TicTacToe_WindowsAPI.cpp
@@ -3,6 +3,7 @@
 
 #include "stdafx.h"
 #include "TicTacToe_WindowsAPI.h"
+#include <windowsx.h>
 
 #define MAX_LOADSTRING 100
 
@@ -75,7 +76,8 @@ ATOM MyRegisterClass(HINSTANCE hInstance)
     wcex.hInstance      = hInstance;
     wcex.hIcon          = LoadIcon(hInstance, MAKEINTRESOURCE(IDI_TICTACTOE_WINDOWSAPI));
     wcex.hCursor        = LoadCursor(nullptr, IDC_ARROW);
-    wcex.hbrBackground  = (HBRUSH)(COLOR_WINDOW+1);
+    //wcex.hbrBackground  = (HBRUSH)(COLOR_WINDOW+1);	//white background
+    wcex.hbrBackground = (HBRUSH)GetStockObject(GRAY_BRUSH);
     wcex.lpszMenuName   = MAKEINTRESOURCEW(IDC_TICTACTOE_WINDOWSAPI);
     wcex.lpszClassName  = szWindowClass;
     wcex.hIconSm        = LoadIcon(wcex.hInstance, MAKEINTRESOURCE(IDI_SMALL));
@@ -121,6 +123,60 @@ BOOL InitInstance(HINSTANCE hInstance, int nCmdShow)
 //  WM_DESTROY  - post a quit message and return
 //
 //
+
+// Global variables for our game
+const int CELL_SIZE = 100;
+
+BOOL GetGameBoardRect(HWND hWnd, RECT *pRect) {
+
+	// Drawing a rectangle on the screen
+	// creating a RECT object
+	RECT rc;
+	// getting the client rectangular structure
+	if (GetClientRect(hWnd, &rc))
+	{
+		int width = rc.right - rc.left;
+		int height = rc.bottom - rc.top;
+
+
+		pRect->left = (width - CELL_SIZE * 3) / 2;
+		pRect->top = (height - CELL_SIZE * 3) / 2;
+		pRect->right = pRect->left + CELL_SIZE * 3;
+		pRect->bottom = pRect->top + CELL_SIZE * 3;
+
+		return true;
+
+	}
+	SetRectEmpty(pRect);
+	return false;
+}
+//Drawing a line between two points!
+void DrawLine(HDC hdc, int x1, int y1, int x2, int y2) {
+	// Setting the drawing pointer to an location
+	MoveToEx(hdc, x1, y1, NULL);
+	// Drawing a line to:
+	LineTo(hdc, x2, y2);
+}
+//getting the cell number from the mouse click
+int GetCellNumberFromPoint(HWND hWnd, int x, int y) {
+
+	POINT pt;
+	RECT rc;
+
+	pt.x = x;
+	pt.y = y;
+	if (GetGameBoardRect(hWnd, &rc)) {
+		// check and see if we at any given point is inside the rectangle
+		if (PtInRect(&rc, pt)){
+			// user clicked inside the game board
+			//Normalize ( 0 to 3*CELL_SIZE)
+
+		}
+	}
+	// return -1 if outside of the game area
+	return -1;
+}
+
 LRESULT CALLBACK WndProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam)
 {
     switch (message)
@@ -142,11 +198,50 @@ LRESULT CALLBACK WndProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam)
             }
         }
         break;
+    case WM_MBUTTONDOWN:
+    {
+	    int xPos = GET_X_LPARAM(lParam);
+	    int yPos = GET_Y_LPARAM(lParam);
+
+	    int index = GetCellNumberFromPoint(hWnd, xPos, yPos);
+
+    }
+    break;
+	   /* Restricting the user to minimize the game beyond the game board*/
+    case WM_GETMINMAXINFO:
+	   {
+	    MINMAXINFO *pMinMax = (MINMAXINFO*)lParam;
+
+	    pMinMax->ptMinTrackSize.x = CELL_SIZE * 5;
+	    pMinMax->ptMinTrackSize.y = CELL_SIZE * 5;
+	    pMinMax->ptMaxTrackSize.x = CELL_SIZE * 6;
+	    pMinMax->ptMaxTrackSize.y = CELL_SIZE * 6;
+	   }
+	   break;
     case WM_PAINT:
         {
             PAINTSTRUCT ps;
             HDC hdc = BeginPaint(hWnd, &ps);
             // TODO: Add any drawing code that uses hdc here...
+		  // Drawing a rectangle on the screen
+		  // creating a RECT object
+		  RECT rc;
+
+		  // getting the client rectangular structure
+		  if (GetGameBoardRect(hWnd, &rc))
+		  {
+			  FillRect(hdc, &rc, (HBRUSH)GetStockObject(WHITE_BRUSH));	//rectangle without shadow
+			  //Rectangle(hdc, rc.left, rc.top, rc.right, rc.bottom);
+		  }
+
+		  for (int i = 0; i < 4; i++)		// use for (int i = 0; i < 4; i++) for only 2 lines
+		  {
+			  //Draw vertical lines
+			  DrawLine(hdc, rc.left + CELL_SIZE * i, rc.top, rc.left + CELL_SIZE * i, rc.bottom);
+			  // draw horizontal line
+			  DrawLine(hdc, rc.left, rc.top + CELL_SIZE * i, rc.right, rc.top + CELL_SIZE * i);
+		  }
+
             EndPaint(hWnd, &ps);
         }
         break;
